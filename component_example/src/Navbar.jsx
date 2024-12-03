ddd9cc436c1bf9fd62783dfbb5197a41670bec5d
Manya Sharma <manya271001@gmail.com>
2024-12-03 12:40:27 +0530

    navbar component

---
+++ component_example/src/Navbar.jsx
@@ -0,0 +1,15 @@
+const Nav=()=>{
+    return(
+        <>
+        <nav className="Navbar">
+            <h1>LoGo</h1>
+            <ul>
+                <li>home</li>
+                <li>service</li>
+                <li>about</li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+export default Nav
